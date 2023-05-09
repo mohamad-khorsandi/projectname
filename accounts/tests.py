d56c1fa8a1d57f94034ef58f2751abde1d00a7ef
mohammad <9m.kh557@gmai.com>
2023-05-09 10:48:18 +0330

    add tests

---
+++ accounts/tests.py
@@ -0,0 +1,25 @@
+import unittest
+
+from accounts.utils import mul, sum, sub, div
+
+
+class TestMathFunctions(unittest.TestCase):
+    def test_mul(self):
+        self.assertEqual(mul(2, 3), 6)
+        self.assertEqual(mul(0, 5), 0)
+        self.assertEqual(mul(-2, -3), 6)
+
+    def test_sum(self):
+        self.assertEqual(sum(2, 3), 5)
+        self.assertEqual(sum(0, 5), 5)
+        self.assertEqual(sum(-2, -3), -5)
+
+    def test_sub(self):
+        self.assertEqual(sub(2, 3), -1)
+        self.assertEqual(sub(0, 5), -5)
+        self.assertEqual(sub(-2, -3), 1)
+
+    def test_div(self):
+        self.assertEqual(div(6, 3), 2)
+        self.assertEqual(div(10, 2), 5)
+        self.assertAlmostEqual(div(1, 3), 0.3333333333333333)
